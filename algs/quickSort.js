f0b3a37f484196f7ec7b67d571c10ca2d807497d
Suzana Zevri <suzana.zevri@gmail.com>
2015-10-01 14:25:41 +0200

    Quick Sort simple implementation

---
+++ algs/quickSort.js
@@ -0,0 +1,25 @@
+var unorderedList = [5, 2, 7, 9, 1, 8, 4];
+var unorderedListChars = ['A', 'D', 'S', 'A', 'B', 'R'];
+
+var quickSort = function(list) {
+    if (list.length <= 2) {
+        return list;
+    }else {
+        var pivot = list[0];
+        var less = [];
+        var greater = [];
+        for (var i = 1; i <= list.length; i++ ){
+            if( list[i] <= pivot ){
+                less.push(list[i]);
+            }
+            if( list[i] > pivot ){
+                greater.push(list[i]);
+            }
+        }
+        return quickSort(less).concat(pivot).concat(greater);
+    }
+
+};
+
+console.log(quickSort(unorderedList))
+console.log(quickSort(unorderedListChars))
